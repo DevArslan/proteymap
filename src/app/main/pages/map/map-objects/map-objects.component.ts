ab4a7e250ac21e3098e526a5ddee2fadcd5d60d4
DevArslan <arslan.gareev.2017@gmail.com>
2020-08-16 01:39:08 +0300

    feat: bind create object function on button

--- src/app/main/pages/map/map-objects/map-objects.component.ts
+++ src/app/main/pages/map/map-objects/map-objects.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ApiService } from "../../../shared/api.service";
+import { ModalService } from '../shared/modal.service';
 
 @Component({
   selector: 'app-map-objects',
@@ -10,11 +11,15 @@ export class MapObjectsComponent implements OnInit {
 
   @Input() objects: any[] = []
 
-  constructor(private API: ApiService) { }
+  constructor(private API: ApiService, private modalService: ModalService) { }
 
   delete(event) {
     const objectId = event.target.parentElement.parentElement.dataset.id
     this.API.deleteObject(objectId)
+  }
+
+  create() {
+    this.modalService.data$.next({ type: 'create', title: 'Добавить объект', state: true, data: {coordinates:{latitude: 0, longitude:0}} })
   }
 
   ngOnInit(): void {
